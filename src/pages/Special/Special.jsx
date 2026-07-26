98f389f7c47ec2a07e0f1a4c15c94ff8dcebce17
FosTer1on <shaha.robot.848@gmail.com>
2026-07-26 21:27:32 +0500

    update fam page

--- src/pages/Special/Special.jsx
+++ src/pages/Special/Special.jsx
@@ -8,7 +8,7 @@ const Special = () => {
         <div className={styles.special_page}>
             <div className={styles.special_block}>
                 <h1>Fazliddin and Shabnam - 100% 👫</h1>
-                <h3>Shabnam Fazliddin Habibulayev uchun juda muxum inson, Shakhzod va Muslim do'stlaridan muhim bob ketishiyam mumkun(yo uje muhumi?). Mayli baxtli bob ketsa boldi♥. Shabnamam juda bemor ekan, xuy e kanakib jidavoti uni, sabr sanga man 11 yil chidavoman.</h3>
+                <h3>Shabnam Fazliddin Habibulayev uchun juda muxum inson, Shakhzod va Muslim do'stlaridan muhim bob ketishiyam mumkun(yo uje muhumi?). Mayli baxtli bob ketsa boldi♥. Shabnamam juda sabrli ekan, xuy ego znaet kanakib chidavoti uni, sabr sanga man 11 yil chidavoman osha uchun mangicha hali bor))).</h3>
             </div>
             <div className={styles.special_block}>
                 <h1>Shakhzod and Hulkar - 50% or less (voobshe xuy ego znaet🙃)</h1>
@@ -19,6 +19,18 @@ const Special = () => {
                 <h3>SOON...</h3>
             </div>
             <p>Sikilib ketdim o'zbe tilida yozurib, shu 1 eban uchun yozdim mani oris divuradigan eban uchun, shu 2 ebandan yaqinro odam yo, this page for you💖</p>
+            <div className={styles.special_block}>
+                <h1>4 Oy o'tdi, yengililar bolid ketdi, yengilila</h1>
+                <h3>1) Proekt endi boshlandi, bugun 26 iyul 2026 soat 21:17, video tashashini kutvoman, keyin targeting ulab boshlimiz. <br />
+                2) Shabnam yoqolib qoldi, Fazlish sikilib yuripti, bugun borib keldi Yangiyolga, Nervoniy bob yuripti, topilib qoladi xudo xolis.i <br />
+                3) Huki sybau boldi, endi Man qisib pulni oylab yuriman, qatdan topsa boladi dib. Sagdiana i Xulkar yetadi Manga, endi yaxshiro pul topib keyin oylimiz. <br />
+                4) Muslimjan topdi i tugadi, endi tugagani yaxshi boldi, yebanutiy qizla keremas, dalbayob xar doim dalbayob boladi eslab qol. <br />
+                5) Inhaga kirdi, kayp, endi Man uk tanladim, Muslim gandon koreya, pidorazdde Xukiyam osha ebanla bilan, mayli glavniy oziga yoqib oqisa boldi, keyin ozi afsus bomasa boldi.
+                </h3>
+            </div>
+            <div className={styles.special_block}>
+                <h3>Endi ishkilib oxshab ketar bu narsa, i glavniy 3 lovimiz yaqin qolili, ozgarib bir birimizga idi naxuy dib ketmirmiz, endi peshanada nma bosa shu boladi, mayli xayirlisi, kodni refactoring qilishda otirganimda yengililani kiritaman shotga, dnevnik boladi mani 🙂</h3>
+            </div>
         </div>
     </Layout>
   )
